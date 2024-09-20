c7e247b32a0954a1120699c5e88f276723960b82
vitebo <andre.vitebo@quero.com>
2024-09-20 10:52:23 -0300

    tmp

--- src/components/QFormOrderByOffer.tsx
+++ src/components/QFormOrderByOffer.tsx
@@ -5,20 +5,35 @@ import QInputRadio from "./QInputRadio";
 const QFormOrderByOffer: FC = () => {
   return (
     <form action="#">
-      <QHeading tag="h2" size="sm" className="mb-2">
+      <QHeading
+        tag="h2"
+        size="sm"
+        className="mb-2"
+      >
         Ordenar
       </QHeading>
-
+      
       <QInputRadio
         label="Cursos de A-Z"
         name="order-by"
         value="course-name"
         checked
+        onChange={ () => {} }
       />
-
-      <QInputRadio label="Menor preço" name="order-by" value="price" />
-
-      <QInputRadio label="Melhor avaliados" name="order-by" value="rating" />
+      
+      <QInputRadio
+        label="Menor preço"
+        name="order-by"
+        value="price"
+        onChange={ () => {} }
+      />
+      
+      <QInputRadio
+        label="Melhor avaliados"
+        name="order-by"
+        value="rating"
+        onChange={ () => {} }
+      />
     </form>
   );
 };
